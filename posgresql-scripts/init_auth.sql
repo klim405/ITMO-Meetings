ec44780d0ccb2f35504c5e6adc4858178dae56ac
klim405 <klim405.develop@gmail.com>
2023-10-03 00:29:53 +0500

    Исправлены примеры базы данных под новые условия person

--- posgresql-scripts/init_auth.sql
+++ posgresql-scripts/init_auth.sql
@@ -2,7 +2,7 @@ create table auth_token (
     token uuid primary key,
     refresh_token uuid unique not null,
     expire_time timestamp with time zone not null
-        default CURRENT_TIMESTAMP + interval '1m',
+        default CURRENT_TIMESTAMP + interval '5m',
     user_id int not null references person (user_id)
         on update CASCADE
         on delete CASCADE
