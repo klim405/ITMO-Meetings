8be5d56c1ddb4e474fcf8c27d0769b0454f000d2
klim405 <klim405.develop@gmail.com>
2023-11-09 22:00:14 +0300

    Добавлены индексы.

--- posgresql-scripts/init.sql
+++ posgresql-scripts/init.sql
@@ -22,6 +22,12 @@ create table person (
         default true
 );
 
+create index person_pk on person (user_id);
+create index person_username on person (username);
+create index person_telephone on person (telephone);
+create index person_email on person (email);
+
+
 create table chanel (
     chanel_id serial primary key,
     name varchar(100) not null,
@@ -34,6 +40,11 @@ create table chanel (
     is_require_confirmation bool not null
         default false
 );
+
+create index chanel_pk on chanel (chanel_id);
+create index chanel_name on chanel (name);
+create index chanel_rating on chanel (rating);
+
 
 create table chanel_member (
     chanel_id int references chanel (chanel_id)
@@ -49,6 +60,10 @@ create table chanel_member (
     notify_about_meeting bool not null
         default false
 );
+
+create index chanel_member_pk on chanel_member (chanel_id, user_id);
+create index chanel_member_permissions on chanel_member (permissions);
+
 
 create table meeting (
     meeting_id serial primary key,
@@ -78,11 +93,27 @@ create table meeting (
 --  todo: rating
 );
 
+create index meeting_pk on meeting (meeting_id);
+create index meeting_chanel_id on meeting (chanel_id);
+create index meeting_title on meeting (title);
+create index meeting_start_time on meeting (start_datetime);
+create index meeting_address on meeting (address);
+create index meeting_capacity on meeting (capacity);
+create index meeting_price on meeting (price);
+create index meeting_minimum_age on meeting (minimum_age);
+create index meeting_maximum_age on meeting (maximum_age);
+create index meeting_for_itmo_students on meeting (only_for_itmo_students);
+create index meeting_for_russians on meeting (only_for_russians);
+
+
 create table category (
     category_id serial primary key,
     title varchar(20) not null
 --  todo: icon
 );
+
+create index category_pk on category (category_id);
+
 
 create table favorite_category (
     user_id int references person (user_id)
@@ -95,6 +126,9 @@ create table favorite_category (
         favorite_categories_pk primary key (user_id, category_id)
 );
 
+create index favorite_category_pk on favorite_category (user_id, category_id);
+
+
 create table meeting_category (
     meeting_id int references meeting (meeting_id)
         on delete CASCADE
@@ -106,8 +140,10 @@ create table meeting_category (
         meeting_category_pk primary key (meeting_id, category_id)
 );
 
+create index meeting_category_pk on meeting_category (meeting_id, category_id);
+
+
 create table feedback (
-    feedback_id bigserial primary key,
     user_id int references person(user_id) 
         on delete CASCADE
         on update CASCADE,
@@ -119,6 +155,9 @@ create table feedback (
     constraint
         one_feedback_per_user unique (user_id, meeting_id)
 );
+
+create index feedback_pk on feedback (user_id, meeting_id);
+
 
 create table meeting_member (
     meeting_id int references meeting(meeting_id) 
@@ -133,8 +172,11 @@ create table meeting_member (
         meeting_member_pk unique (meeting_id, user_id)
 );
 
+create index meeting_member_pk on meeting_member (meeting_id, user_id);
+
+
 create table passport_rf (
-    number bigint  primary key,
+    number bigint primary key,
     user_id int references person(user_id)
         on delete CASCADE
         on update CASCADE,
@@ -146,10 +188,16 @@ create table passport_rf (
     who_give text not null
 );
 
+create index passport_rf_user_id on passport_rf (user_id);
+
+
 create table country (
     country_id serial primary key,
     title varchar(60) not null
 );
+
+create index country_pk on country(country_id);
+
 
 create table passport (
     number varchar(10) not null,
@@ -170,12 +218,17 @@ create table passport (
         passport_pk primary key (number, country_id)
 );
 
+create index passport_user_id on passport (user_id);
+
+
 create table car (
     number varchar(10) primary key,
-    user_id int references person(user_id)
+    user_id int references person (user_id)
         on delete CASCADE
         on update CASCADE,
     country_id int references country(country_id)
         on delete CASCADE
         on update CASCADE
 );
+
+create index car_user_id on car (user_id);
