fa64d07e61227e84bfd1b70342364877bd75e19c
klim405 <klim405.develop@gmail.com>
2023-11-11 03:40:03 +0300

    Исправлена проблема с маршрутизацией.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -20,6 +20,6 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
 
     from app.api import api as api_blueprint
-    app.register_blueprint(api_blueprint)
+    app.register_blueprint(api_blueprint, url_prefix='/api')
 
     return app
