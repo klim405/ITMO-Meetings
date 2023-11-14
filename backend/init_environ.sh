4a5a6228492bf178fa81803b0dcb80e31a6d1f3f
klim405 <klim405.develop@gmail.com>
2023-11-14 23:34:35 +0300

    Добавлен файл экспорта переменных среды.

---
+++ backend/init_environ.sh
@@ -0,0 +1,12 @@
+# секретный ключ
+export SECRET_KEY="/"
+# Почта администратора
+export FLASKY_ADMIN="/"
+# Почта сервера
+export MAIL_USERNAME="/"
+# Пароль от почты сервера
+export MAIL_PASSWORD="/"
+
+export DB_DEVELOPMENT_URI="postgresql://test:test@localhost:5432/tests"
+export DB_TEST_URI="postgresql://test:test@localhost:5432/tests"
+export DB_PRODUCTION_URI=""
