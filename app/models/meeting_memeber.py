2b70a47b6186a83dcbfc94c088284973d0ef45e9
klim405 <klim405.develop@gmail.com>
2025-01-04 16:01:46 +0300

    fix: Исправлена модель MeetingMember.

--- app/models/meeting_memeber.py
+++ app/models/meeting_memeber.py
@@ -10,7 +10,7 @@ class MeetingMember(Base):
     __table_args__ = {"extend_existing": True}
     user_id = mapped_column(Integer, ForeignKey("person.user_id"), primary_key=True)
     meeting_id = mapped_column(Integer, ForeignKey("meeting.meeting_id"), primary_key=True)
-    date_of_join = mapped_column(DateTime, nullable=False, default=datetime_now)
+    date_of_join = mapped_column(DateTime(timezone=True), nullable=False, default=datetime_now)
 
     user = relationship("User")
     meeting = relationship("Meeting")
